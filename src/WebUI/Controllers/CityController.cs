5157741153d8f384b8c95549502611b8725a295c
BEQA <beqa.nasuashvili@live.com>
2022-02-28 15:02:38 +0400

    initial commit

---
+++ src/WebUI/Controllers/CityController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ZenAchitecture.Application.Account.Cities.Dtos;
+using ZenAchitecture.Application.Account.Cities.Queries;
+using ZenAchitecture.Application.Account.Cities.Commands;
+using Zen.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ZenAchitecture.WebUI.Controllers.V1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class CityController : ZenController
+    {
+        public CityController(ILogger<CityController> logger)
+        {
+            logger.LogInformation("Test LogInformation");
+            logger.LogWarning("Test LogWarning");
+            logger.LogError("Test LogError");
+            logger.LogCritical("Test LogCritical");
+            logger.LogTrace("Test LogTrace");
+        }
+
+        [HttpGet]
+        [Route(nameof(ReadCity))]
+        public async Task<CityDto> ReadCity() => await Mediator.Send(new GetCityQuery());
+
+        [HttpGet]
+        [Route(nameof(GetCities))]
+        public async Task<List<CityDto>> GetCities() => await Mediator.Send(new GetCitiesQuery());
+
+        [HttpPost]
+        [Route(nameof(CreateCity))]
+        public async Task<int> CreateCity(CreateCityCommand command) => await Mediator.Send(command);
+
+        [HttpPost]
+        [Route((nameof(UpdateCity)))]
+        public async Task<int> UpdateCity(UpdateCityCommand command) => await Mediator.Send(command);
+
+        [HttpDelete]
+        [Route((nameof(DeleteCity)))]
+        public async Task<int> DeleteCity([FromQuery] DeleteCityCommand command) => await Mediator.Send(command);
+
+    }
+}
